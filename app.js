1754bca8fc32d6dc502108efc4474256b33a734e
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-05-12 22:34:29 -0700

    changed port number

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app     = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-PORT        = 50688;
+PORT        = 50689;
 
 // Database
 var db = require('./database/db-connector');
