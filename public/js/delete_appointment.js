422b52a44bcf8c9a523a89b990653fa52ef7dbba
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-05-15 10:38:16 -0400

    created delete appointment js

---
+++ public/js/delete_appointment.js
@@ -0,0 +1,42 @@
+function deleteAppointment(appointmentID) {
+    let appointmentData = {
+        appointmentID: appointmentID
+    };
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-appointment", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+            deleteRow(appointmentID);
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input");
+        }
+    }
+    xhttp.send(JSON.stringify(appointmentData));
+};
+
+function deleteRow(appointmentID)
+{
+    let table = document.getElementById("appointments-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        if (table.rows[i].getAttribute("appointment-data-value") == appointmentID) {
+            table.deleteRow(i);
+            deleteDropDownMenu(appointmentID);
+            break;
+        }
+    }
+}
+
+function deleteDropDownMenu(appointmentID) {
+    let selectMenu = document.getElementById("appointmentSelect");
+    for (let i = 0; i < selectMenu.clientHeight; i++) {
+        if(Number(selectMenu.options[i].value) === Number(appointmentID)) {
+            selectMenu[i].remove();
+            break;
+        }
+    }
+}
