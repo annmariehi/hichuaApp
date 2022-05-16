7ac94cee9b5c65bb3e8a189947798e9a0a2a79dd
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-05-16 18:41:57 -0400

    added view buttons/function for procedures

--- public/js/view_vet-proc.js
+++ public/js/view_vet-proc.js
@@ -1,26 +1,26 @@
 // setting the features for the procedures list popup
 let windowFeatures = "popup, left=100, top=100, width=320, height=320";
 
-// appointmentID passed from appointments table with "view" button and viewAppt({{this.appointmentID}})
-function viewAppt(apptID){
+// vetID passed from Veterinarians table with "view" button and viewProcedures({{this.vetID}})
+function viewProcedures(vetID){
 
-	let apptProcData = {
-		appointmentID: apptID
+	let vetProcData = {
+		vetID: vetID
 	}
 
 	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "/view-appt-procs", true);
+	xhttp.open("POST", "/view-vet-procs", true);
 	xhttp.setRequestHeader("Content-type", "application/json");
 
 	xhttp.onreadystatechange = () => {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			// if sent successfully, open popup of view-appt-procs, called by GET
-			this.window.open("/view-appt-procs", "ViewProcs", windowFeatures);
+			this.window.open("/view-vet-procs", "ViewProcs", windowFeatures);
 		}
 		else if(xhttp.readyState == 4 && xhttp.status !=200) {
 			console.log("There was an error with the input");
 		}
 	}
-	xhttp.send(JSON.stringify(apptProcData));
+	xhttp.send(JSON.stringify(vetProcData));
 }
 
