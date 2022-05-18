fae76f4f1ef368141fda4451d79c3e0aa29bd8aa
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-05-17 20:48:20 -0400

    added update owners functionality

--- public/js/update_owner.js
+++ public/js/update_owner.js
@@ -1,4 +1,4 @@
-// get objects we need to modify
+
 let updateOwnerForm = document.getElementById('update-owner-form');
 
 // modify objects we need
@@ -17,27 +17,19 @@ updateOwnerForm.addEventListener("submit", function (e) {
 
     // for REQUIRED values
 
-    if (isNaN(emailValue))
-    {
-        return;
-    }
 
     // put data we want to send in javascript object
-    let Owner = {
+    let ownerData = {
         ownerID: ownerIDValue,
         email: emailValue
     }
 
-    // setup ajax request
     var xhttp = new XMLHttpRequest();
     xhttp.open("PUT", "/put-owner", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // tell ajax how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-            // add new data to table
             updateRow(xhttp.response, ownerIDValue);
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -46,26 +38,20 @@ updateOwnerForm.addEventListener("submit", function (e) {
     }
 
     // send request and wait for response
-    xhttp.send(JSON.stringify(Owner));
+    xhttp.send(JSON.stringify(ownerData));
 
 })
 
-function updateRow(Owner, ownerID){
-    let parseData = JSON.parse(Owner);
-
+function updateRow(ownerData, ownerIDVal){
+    let parseData = JSON.parse(ownerData);
     let table = document.getElementById("owners-table");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
         // iterate thru rows
-        if (table.rows[i].getAttribute("data-value") == ownerID) {
+        if (table.rows[i].getAttribute("data-value") == ownerIDVal) {
 
-            // get location of row where matching pet ID is
             let updateRowIndex = table.getElementsByTagName("tr")[i];
-
-            // get td of pet_type value
             let td = updateRowIndex.getElementsByTagName("td")[2];
-
-            // reassign pet_type to value we updated to
             td.innerHTML = parseData[0].email;
         }
     }
