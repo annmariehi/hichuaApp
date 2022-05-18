59fa331930e69d62748df39c439cf9cc652dd24d
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-05-18 12:39:57 -0400

    fixed comments

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,4 +1,4 @@
-// Citation for following page:
+// Citation for handlebar helper set up (not helpers themselves)
 // Date: 05/14/2022
 // Adapted from:
 // Source: https://web.archive.org/web/20170825085136/http://www.codyrushing.com:80/using-handlebars-helpers-on-both-client-and-server
